1150ffe1fae049db791a671e3b4505d3fe823dd4
kenkleven <kenkleven50@gmail.com>
2024-09-15 14:18:02 +0100

    add nltk

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
 
+nltk.download('all')
 # Initialiser le lemmatizer
 lemmatizer = WordNetLemmatizer()
 
